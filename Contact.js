8bf446eeb34b4c044d92376843b8c920d5c06384
jaysi1701 <jaysigaur9449@gmail.com>
2026-06-09 15:53:40 +0530

    Implemented UC4 edit existing contact

--- Contact.js
+++ Contact.js
@@ -1,6 +1,37 @@
 class Contact {
 
     constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+
+        let nameRegex = /^[A-Z][a-zA-Z]{2,}$/;
+        let addressRegex = /^[A-Za-z ]{4,}$/;
+        let zipRegex = /^[0-9]{6}$/;
+        let phoneRegex = /^[0-9]{10}$/;
+        let emailRegex = /^[a-zA-Z0-9]+([._+-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/;
+
+        if (!nameRegex.test(firstName))
+            throw "Invalid First Name";
+
+        if (!nameRegex.test(lastName))
+            throw "Invalid Last Name";
+
+        if (!addressRegex.test(address))
+            throw "Invalid Address";
+
+        if (!addressRegex.test(city))
+            throw "Invalid City";
+
+        if (!addressRegex.test(state))
+            throw "Invalid State";
+
+        if (!zipRegex.test(zip))
+            throw "Invalid Zip Code";
+
+        if (!phoneRegex.test(phoneNumber))
+            throw "Invalid Phone Number";
+
+        if (!emailRegex.test(email))
+            throw "Invalid Email";
+
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
--- addressBook.js
+++ addressBook.js
@@ -1,14 +1,61 @@
 const Contact = require('./Contact');
 
-let person1 = new Contact(
-    "John",
-    "Doe",
-    "Anna Nagar",
-    "Chennai",
-    "Tamil Nadu",
-    "600001",
-    "9876543210",
-    "john@gmail.com"
-);
+let addressBook = [];
 
-console.log(person1.toString());
+try {
+
+    let contact1 = new Contact(
+        "John",
+        "Doe",
+        "Anna Nagar",
+        "Chennai",
+        "TamilNadu",
+        "600001",
+        "9876543210",
+        "john@gmail.com"
+    );
+
+    let contact2 = new Contact(
+        "David",
+        "Smith",
+        "MGRoad",
+        "Bangalore",
+        "Karnataka",
+        "560001",
+        "9876543211",
+        "david@gmail.com"
+    );
+
+    addressBook.push(contact1);
+    addressBook.push(contact2);
+
+    console.log("Before Editing:");
+    addressBook.forEach(contact =>
+        console.log(contact.toString())
+    );
+
+    // UC4 - Find and Edit Contact
+
+    let person = addressBook.find(
+        contact =>
+            contact.firstName === "John" &&
+            contact.lastName === "Doe"
+    );
+
+    if (person) {
+        person.address = "Velachery";
+        person.city = "Chennai";
+        person.state = "TamilNadu";
+        person.zip = "600042";
+        person.phoneNumber = "9999999999";
+        person.email = "john.doe@gmail.com";
+    }
+
+    console.log("\nAfter Editing:");
+    addressBook.forEach(contact =>
+        console.log(contact.toString())
+    );
+
+} catch (error) {
+    console.error(error);
+}
